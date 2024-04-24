dd7a417c4d767f21149527fbb77094fd5bb9f3ee
Vincent <vincent.chauvaux@gmail.com>
2024-04-24 12:33:24 +0200

    ajout du 1er commit du cours 2/2

---
+++ src/pages/Blog.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation"
+
+const Blog = () => {
+    return (
+        <div className="blog-container">
+            <Logo />
+            <Navigation />
+            <h1>Logo</h1>
+        </div>
+    );
+};
+
+export default Blog;
